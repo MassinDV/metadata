3e17b2e0ede7586e5bc71d67e69eef74b2fa2690
Massin <40926996+MassinDV@users.noreply.github.com>
2025-03-05 03:21:18 +0100

    Update epgroya.php

--- asharq/epgroya.php
+++ asharq/epgroya.php
@@ -104,7 +104,7 @@ for ($page = 1; $page <= 6; $page++) {
 
 // Save XML to file
 $doc->formatOutput = true;
-$doc->save('./epg/royaepg.xml');
+$doc->save('royaepg.xml');
 
 echo "EPG file has been created successfully.\n";
 
